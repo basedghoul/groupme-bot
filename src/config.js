05868f93d40fca548da5c46e9badfee2e9cf1e8e
basedghoul <schelale17@gmail.com>
2017-04-12 20:32:16 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,1 +1,19 @@
+var config = {};
 
+config.bots = [
+  {
+    botID: '3706613804657289cb5f13f8be',
+    groupID: '30416051',
+    groupLocalID: '1', // an identifier for this group used in this application only
+    botName: 'Weekly TGIF',
+    groupName: 'the-group-name' // optional
+  }
+];
+
+config.yahooWeatherAppId = '';
+
+// Alex CI user token
+config.alexUserToken = 'Azv38MEp1LwnCB1NVnlXyyCoYDecBh0i1jR70TYp
+';
+
+module.exports = config;
